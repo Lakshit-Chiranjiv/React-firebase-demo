bb0f44ddd5ba5ca24c52f2cb8d845beb0f36c936
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-02-22 17:05:24 +0530

    made improvement for errors and started implementing update operation

--- react firebase demo app/src/components/DbInterface.jsx
+++ react firebase demo app/src/components/DbInterface.jsx
@@ -1,16 +1,23 @@
 import React, { useState,useEffect } from 'react';
 import { db } from './../firebase-config';
-import { collection,getDocs,addDoc } from 'firebase/firestore';
-import { async } from '@firebase/util';
+import { collection,getDocs,addDoc,setDoc,doc,updateDoc } from 'firebase/firestore';
 
 const DbInterface = () => {
 
-    let foodID = 1;
     const [foodItems,setFoodItems] = useState([]);
     
     const [itemName,setItemName] = useState("");
     const [itemPrice,setItemPrice] = useState(0);
     const [itemAvailability,setItemAvailability] = useState(true);
+
+    const [updateItemName,setUpdateItemName] = useState("");
+    const [updateItemPrice,setUpdateItemPrice] = useState(0);
+    const [updateItemAvailability,setUpdateItemAvailability] = useState(true);
+
+    const [showAddEmptyError,setShowAddEmptyError] = useState(false);
+    const [showAddExistError,setShowAddExistError] = useState(false);
+    const [showUpdateEmptyError,setShowUpdateEmptyError] = useState(false);
+    const [showUpdateExistError,setShowUpdateExistError] = useState(false);
 
     const foodMenuCollectionRef = collection(db,"food_menu");
 
@@ -26,9 +33,59 @@ const DbInterface = () => {
 
     const submitMenuItem = async(e)=>{
         e.preventDefault();
+        if(itemName !== ''){
+            let tempFoodItems = foodItems.map((val)=>val.item.toLowerCase());
+            if(!tempFoodItems.includes(itemName.toLowerCase())){
+                await setDoc(doc(foodMenuCollectionRef, String(foodItems.length + 1)),{
+                    item: itemName,
+                    price: itemPrice,
+                    available: Boolean(itemAvailability)
+                });
+                getMenu();
+            }
+            else{
+                setShowAddExistError(true);
+                setTimeout(()=>{
+                    setShowAddExistError(false);
+                },4000);
+            }
+        }
+        else{
+            setShowAddEmptyError(true);
+            setTimeout(()=>{
+                setShowAddEmptyError(false);
+            },4000);
+        }
         // console.log(itemName,itemPrice,typeof(itemAvailability),itemAvailability);
-        await addDoc(foodMenuCollectionRef,{ item: itemName,price: itemPrice,available: Boolean(itemAvailability) });
-        getMenu();
+        // await addDoc(foodMenuCollectionRef,{ item: itemName,price: itemPrice,available: Boolean(itemAvailability) });
+        
+    }
+
+    const updateMenuItem = async(e)=>{
+        e.preventDefault();
+        if(updateItemName !== ''){
+            let tempFoodItems = foodItems.map((val)=>val.item.toLowerCase());
+            if(tempFoodItems.includes(itemName.toLowerCase())){
+                await setDoc(doc(foodMenuCollectionRef, String(foodItems.length + 1)),{
+                    item: itemName,
+                    price: itemPrice,
+                    available: Boolean(itemAvailability)
+                });
+                getMenu();
+            }
+            else{
+                setShowUpdateExistError(true);
+                setTimeout(()=>{
+                    setShowUpdateExistError(false);
+                },4000);
+            }
+        }
+        else{
+            setShowUpdateEmptyError(true);
+            setTimeout(()=>{
+                setShowUpdateEmptyError(false);
+            },4000);
+        }
     }
 
     useEffect(()=>{
@@ -38,13 +95,13 @@ const DbInterface = () => {
   return (
     <>
     <h1 className='text-center m-3 display-1'>Firebase Restaurent</h1>
-    <div className='db'>
+    <div className='mb-5 db'>
         <div className="menu">
             <h1>Food Menu</h1>
             {
                 foodItems.map((food)=>{
                     return (
-                        <div className="itm" key={food.itemId}>
+                        <div className="m-4 itm" key={food.itemId}>
                             <h4 className={food.available?'text-success':'text-warning'}>{food.item}....${food.price}</h4>
                             <button className='btn btn-danger'>Delete</button>
                         </div>
@@ -64,13 +121,49 @@ const DbInterface = () => {
                 <br /><br />
                 <label htmlFor="availability">Available??</label>
                 <select name="avail" onChange={(e)=>{
-                    console.log("hey");
                     setItemAvailability(e.target.value==="true")}}>
                     <option value="true">True</option>
                     <option value="false">False</option>
                 </select>
 
                 <input type="submit" value="submit" />
+                <br />
+                {
+                    showAddEmptyError && 
+                    <p className='text-danger'>Please fill the item name !!</p>
+                }
+                {
+                    showAddExistError &&
+                    <p className='text-danger'>This item already exists in the menu!!</p>
+                }
+                
+            </form>
+
+            <h1>Update Menu Item</h1>
+
+            <form onSubmit={updateMenuItem}>
+                <label htmlFor='item name'>Food Item Name</label>
+                <input type="text" placeholder='item name ....' onChange={(e)=>{setUpdateItemName(e.target.value)}}/>
+                <br /><br />
+                <label htmlFor='item price'>Food Item Price</label>
+                <input type="number" placeholder='price...' onChange={(e)=>{setUpdateItemPrice(Number(e.target.value))}}/>
+                <br /><br />
+                <label htmlFor="availability">Available??</label>
+                <select name="avail" onChange={(e)=>{
+                    setUpdateItemAvailability(e.target.value==="true")}}>
+                    <option value="true">True</option>
+                    <option value="false">False</option>
+                </select>
+                <input type="submit" value="Update" />
+                <br />
+                {
+                    showUpdateEmptyError && 
+                    <p className='text-danger'>Please fill the item name !!</p>
+                }
+                {
+                    showUpdateExistError &&
+                    <p className='text-danger'>This item doesn't exists in the menu!!</p>
+                }
             </form>
         </div>
     </div>
